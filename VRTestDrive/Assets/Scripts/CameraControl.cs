623774dc0d66434b8c21a4b5d3bb39666b06ecff
Craig Stone <craigwstone@gmail.com>
2018-10-21 11:35:24 -0400

    Refined physics

--- VRTestDrive/Assets/Scripts/CameraControl.cs
+++ VRTestDrive/Assets/Scripts/CameraControl.cs
@@ -2,20 +2,30 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraControl : MonoBehaviour {
- 
-        public GameObject player;       //Public variable to store a reference to the player game object
+public class CameraControl : MonoBehaviour
+{
 
+    public GameObject player;       //Public variable to store a reference to the player game object
 
-        private Vector3 offset;         //Private variable to store the offset distance between the player and camera
+    public Rigidbody playerBody;    //Public variable to store a reference to the player's Rigidbody
+
+    private Vector3 offset;         //Private variable to store the offset distance between the player and camera
 
     // Use this for initialization
-    void Start () {
+    void Start()
+    {
         offset = transform.position - player.transform.position;
-	}
-	
-	// LateUpdate is called once per frame after update
-	void LateUpdate () {
+        playerBody = player.GetComponent<Rigidbody>();
+    }
+
+    // LateUpdate is called once per frame after update
+    void LateUpdate()
+    {
         transform.position = player.transform.position + offset;
-	}
+    }
+
+    void movePlayer()
+    {
+
+    }
 }
--- VRTestDrive/Assets/Scripts/GravityController.cs
+++ VRTestDrive/Assets/Scripts/GravityController.cs
@@ -2,7 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GravityController : MonoBehaviour {
+public class GravityController : MonoBehaviour
+{
 
     //public Vector3 g = Physics.gravity;
     public Rigidbody rb;
@@ -12,23 +13,25 @@ public class GravityController : MonoBehaviour {
     bool jump = false;
     float jmp = 0f;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start()
+    {
         Cursor.visible = true;
         rb = GetComponent<Rigidbody>();
         coll = GetComponent<Collider>();
         pos = rb.position;
         mousepos = new MouseLook();
 
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        Debug.Log(Input.GetAxis("Horizontal") + "V" + Input.GetAxis("Vertical"));
-        jump = Input.GetKeyDown("joystick button 0");
-        if (jump)
-            jmp = 1;
-        rb.AddForce(Input.GetAxis("Horizontal"),jmp, Input.GetAxis("Vertical"), ForceMode.Force);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Debug.Log(Input.GetAxis("Jump"));
+        //jump = Input.GetKeyDown("Jump");
+        //if (jump)
+        //    jmp = 1;
+        rb.AddForce(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical"), ForceMode.Force);
     }
 
     void OnMouseDrag()
@@ -37,7 +40,7 @@ public class GravityController : MonoBehaviour {
         //pos.y = ((Input.mousePosition.y) / 900);
         //pos = mousepos.MousePos();
         //coll.transform.position = pos;
-        Debug.Log(Input.GetAxis("Horizontal") + "V" + Input.GetAxis("Vertical"));
-        
+        //Debug.Log(Input.GetAxis("Horizontal") + "V" + Input.GetAxis("Vertical"));
+
     }
 }
