0645ded759d88050bf39d7342ff16c993003a371
Craig <craigwstone@gmail.com>
2018-10-20 23:48:59 -0400

    Probably the worst mouse controls I have ever done but it barely works

---
+++ VRTestDrive/Assets/Scripts/GravityController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GravityController : MonoBehaviour {
+
+    public Vector3 g = Physics.gravity;
+    Rigidbody rb;
+    Collider coll;
+    Vector3 pos;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+        coll = GetComponent<Collider>();
+        pos = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnMouseDrag()
+    {
+        pos.x = ((Input.mousePosition.x - 1440) / 1440);
+        pos.y = ((Input.mousePosition.y) / 900);
+        coll.transform.position = pos;
+    }
+}
---
+++ VRTestDrive/Assets/Scripts/MouseLook.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+    public Camera cam;
+
+    // Use this for initialization
+    void Start () {
+        cam = GetComponent<Camera>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public Vector3 MousePos()
+    {
+
+
+        return new Vector3(0f,0f,0f);
+    }
+}
