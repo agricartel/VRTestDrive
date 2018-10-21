1cdd806fb1f462fca7f9b076f89f7c0f79f364c5
jpham4@gmail.com <jpham4@gmail.com>
2018-10-21 12:52:50 -0400

    implemented jumping and floating

---
+++ VRTestDrive/Assets/Scripts/Jump.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jump : MonoBehaviour {
+    Vector3 jumpHeight = new Vector3(0.0f, 0.2f, 0.0f);
+
+    // Use this for initialization
+    void Start () {
+    
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButton("Jump")) {
+            transform.position += jumpHeight;
+        }
+	}
+}
